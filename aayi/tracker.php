df47a515a4a4b7766ff5fc1bd7e3d5afbaca0cb5
josephernest <nouvellecollection@gmail.com>
2020-06-04 15:31:45 +0200

    Delete tracker.php

--- aayi/tracker.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$txt = time() . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '') . "\t" . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '') . PHP_EOL;
-file_put_contents('aayi/aayi.log', $txt, FILE_APPEND);
-?>
